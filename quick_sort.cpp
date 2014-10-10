e8c9572328a0e3a227612fda826b95949f8ae988
lunawyll <lunawyll@mail.ru>
2014-10-11 00:13:35 +0400

    quick_sort: 1) после partition происходила утеря местонахождения элемента, по которому надо делить, поэтому вначале надо поставить его, скажем, в конец, а потом поменять местами с элеменом по индексу, который возвращает partition 2) partition возвращает место pivot, а не то, что было сказано до этого

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -7,8 +7,8 @@ using namespace std;
 // pivot - "опорный" элемент
 // partition - переупорядочивает элементы части массива,
 // заданной отрезком [left, right), так что в начале
-// следуют элементы меньшие pivot, а в конце - большие;
-// возвращает место начала блока элементов, больших pivot;
+// следуют элементы меньшие pivot, а в конце  - большие;
+// возвращает место pivot;
 int * partition(int * left, int *  right, int pivot) {
 	int * store = left; // место для вставки элементов, меньших pivot
 	for (int * p = left; p != right; ++p)
@@ -17,19 +17,35 @@ int * partition(int * left, int *  right, int pivot) {
 	return store;
 }
  
+
 void my_qsort(int * arr, int n) {
 	if (n <= 1)
 		return; // массив в 1 или 0 элементов уже упорядочен
 	int * pivotPtr = arr + rand() % n; // случайный выбор опорного элемента
-	int newPivotIdx = partition(arr, arr + n, *pivotPtr) - arr;
-	my_qsort(arr, newPivotIdx + 1);
-	my_qsort(arr + newPivotIdx, n - (newPivotIdx + 1));
+	swap(arr[n - 1], *pivotPtr);
+	int newPivotIdx = partition(arr, arr + n, arr[n - 1]) - arr;
+	swap(arr[newPivotIdx], arr[n - 1]);
+	my_qsort(arr, newPivotIdx);
+	my_qsort(arr + newPivotIdx + 1, n - (newPivotIdx + 1));
 }
 
 
 int main(){
-
-
-
+	srand((unsigned)time(0));
+	const int n = 10;
+	//int a[n] {4, 5, 2, 7, 1};
+	int a[n];
+	
+	for (int i = 0; i < n; i++){
+		a[i] = rand() % 100;
+	}
+	
+	for (int i = 0; i < n; i++)
+		cout << a[i] << ' ';
+	my_qsort(a, n);
+	cout << endl;
+	for (int i = 0; i < n; i++)
+		cout << a[i] << ' ';
+	cout << endl;
 	return 0;
 }
